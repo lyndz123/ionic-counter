e8998681a5ee0a823ea0cb798c9573bfee5f1def
Ian <mcloughlin.ian@gmail.com>
2015-02-25 23:40:27 +0000

    Added counter logic

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,5 @@
 angular.module('counter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+.controller("CountCtrl", function($scope) {
+  $scope.count = 0;
+});
